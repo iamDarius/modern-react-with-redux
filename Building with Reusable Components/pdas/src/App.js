3bc5e58f09d2182d95448daaea0d928bf589d2c0
Darius Moore <dmoore@costar.com>
2022-11-23 14:37:02 -0500

    Showing images in profile component

--- Building with Reusable Components/pdas/src/App.js
+++ Building with Reusable Components/pdas/src/App.js
@@ -1,12 +1,14 @@
 import ProfileCard from "./ProfileCard";
-
+import AlexaImage from './images/alexa.png';
+import CortanaImage from './images/cortana.png';
+import SiriImage from './images/siri.png';
 function App() {
     return (
         <div>
-            <div> Personal Digital Assisntants</div>
-            <ProfileCard />
-            <ProfileCard />
-            <ProfileCard />
+            <div><h1>Personal Digital Assistants</h1></div>
+            <ProfileCard title="Alexa" handle="@alexa89" image={AlexaImage} />
+            <ProfileCard title="Cortana" handle="@cortana32" image={CortanaImage} />
+            <ProfileCard title="Siri" handle="@siri01" image={SiriImage} />
         </div>
     );
 }
--- Building with Reusable Components/pdas/src/ProfileCard.js
+++ Building with Reusable Components/pdas/src/ProfileCard.js
@@ -1,5 +1,11 @@
-function ProfileCard() {
-    return <div> Profile Card!!</div>;
+
+function ProfileCard({title, handle, image}) {
+    return(
+        <div>
+            <div><h3>{title}</h3></div>
+            <img alt="pda logo" src={image} />
+        </div>
+    ) ;
 }
 
 export default ProfileCard;
